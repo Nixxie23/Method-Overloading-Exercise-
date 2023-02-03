97bfc0c8bd7ccc45accc4480e7e6972e9ca8aa46
taylor Schuchter <taylorschuchter@gmail.com>
2023-02-03 15:46:59 -0500

    Add methods completed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace MethodOverloading
+﻿using System.Transactions;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int x, int y)
+        { return x + y; }
+
+        public static decimal Add(decimal x, decimal y)
+        { return x + y; }
+
+        public static string Add(int x, int y, bool isTrue)
+        {
+
+            if (isTrue == true && x + y == -1)
+            {
+                return $"{x + y} dollar";
+            }
+            else if (isTrue == true && x + y == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else if (isTrue == true && x + y > 1 || x + y <= 0)
+            {
+                return $"{x + y} dollars";
+            }
+            else
+                return "";
+
+}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{Add(1, 2)}");
+            Console.WriteLine($"{Add(1.3m, 2.5m)}");
+            Console.WriteLine($"{Add(1, 2, true)}");
+            Console.WriteLine($"{Add(1, 0, true)}");
+            Console.WriteLine($"{Add(1, -5, true)}");
+            Console.WriteLine($"{Add(1, -2, true)}");
+            Console.WriteLine($"{Add(1, 2, false)}");
         }
     }
 }
